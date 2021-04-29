125cd4182ce892b57247e184b63b37f927e018ed
JS pervaya zadaniye <js1@gmail.com>
2021-04-29 07:01:15 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+var user_name = prompt('Введите своё имя: ');
+console.log('Имя пользователя: ' + user_name);
+
+var first = '6 + 9 = '
+var second = '6 * 3 = '
+var third = '6 / 2 = '
+var fourth = '6 - 4 = '
+
+var first2 = 6 + 9 
+var second2 = 6 * 3
+var third2 = 6 / 2
+var fourth2 = 6 - 4
+
+
+    var first1 = +prompt(first);
+    console.log('Правильный ответ: ' + first + first2 + ' (Ваш ответ: ' + first1 +' )' );
+
+
+
+  var second1 = +prompt(second);
+    console.log('Правильный ответ: ' + second + second2 + ' (Ваш ответ: ' + second1 +' )' );
+
+
+
+
+    var third1 = +prompt(third);
+    console.log('Правильный ответ: ' + third + third2 + ' (Ваш ответ: ' + third1 +' )' );
+
+
+
+    var fourth1 = +prompt(fourth);
+    console.log('Правильный ответ: ' + fourth + fourth2 + ' (Ваш ответ: ' + fourth1 +' )' );
+    alert('Зайдите в консоль что бы узнать ответы <F12/Console>');
+
+
+
+    
